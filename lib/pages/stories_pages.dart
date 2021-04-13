c3704e5f5e6b1fc4b1ca6695d8cde7bbc73a8864
Abdelghani Meliani <a.meliani@esi-sba.dz>
2021-04-13 20:58:22 +0200

    stories detail page

--- lib/pages/stories_pages.dart
+++ lib/pages/stories_pages.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:snapshat_clone/data/stories_json.dart';
+import 'package:snapshat_clone/pages/story_detail.dart';
 import 'package:snapshat_clone/theme/colors.dart';
 
 class StoriesPage extends StatefulWidget {
@@ -22,13 +25,104 @@ class _StoriesPageState extends State<StoriesPage> {
     var size = MediaQuery.of(context).size;
     return Container(
       decoration: BoxDecoration(
-          color: white,
           borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(10),
-            bottomRight: Radius.circular(10),
-          )),
-      child: Column(
-        children: [],
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+          color: white),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 20, right: 10),
+              child: Text(
+                "For You",
+                style: TextStyle(
+                    fontSize: 16, color: black, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 10, right: 10, top: 10, bottom: 10),
+              child: Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                children: List.generate(stories_data.length, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          PageTransition(
+                              type: PageTransitionType.scale,
+                              alignment: Alignment.bottomCenter,
+                              child: StoryDetailPage(
+                                videoUrl: stories_data[index]['videoUrl'],
+                              )));
+                    },
+                    child: Container(
+                      width: (size.width - 30) / 2,
+                      height: 250,
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: (size.width - 30) / 2,
+                            height: 250,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        (stories_data[index]['img'])),
+                                    fit: BoxFit.cover)),
+                          ),
+                          Container(
+                            width: (size.width - 15) / 2,
+                            height: 250,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                    colors: [
+                                      black.withOpacity(0.25),
+                                      black.withOpacity(0),
+                                    ],
+                                    end: Alignment.topCenter,
+                                    begin: Alignment.bottomCenter)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10),
+                                  child: Text(
+                                    stories_data[index]['name'],
+                                    style: TextStyle(
+                                        color: white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 5, left: 10, bottom: 10),
+                                  child: Text(
+                                    stories_data[index]['date'],
+                                    style: TextStyle(
+                                        color: white.withOpacity(0.7),
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
